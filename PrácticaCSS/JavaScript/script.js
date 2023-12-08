c84d8a894aecb639c53a069821493369aff55d7c
jemilianomg <madrigaljesusemiliano@gmail.com>
2023-12-08 16:07:59 -0600

    08Diciembre2023

---
+++ PrácticaCSS/JavaScript/script.js
@@ -0,0 +1,67 @@
+//comentarios de una sola linea
+
+/*comentarios multilineales*/
+
+
+
+//alert("Hola mundo")
+
+
+//conosle.log("Este es un mensaje para mi consola")
+
+
+/*var nombre= prompt("Ingresa tu nombre");
+alert("Hola, "  + nombre +  "Bienvenido a la ch35");*/
+
+
+//var numero1=parseInt(prompt("Ingresa el primer número:"));
+//var numero2=parseInt(prompt("Ingresa el segundo número:"));
+
+/*console.log("suma:"+(numero1+numero2));
+console.log("resta:"+(numero1-numero2));
+console.log("multiplicación:"+(numero1*numero2));
+console.log("división:"+(numero1/numero2));*/
+
+
+//Pedir al ususario que ingrese la edad
+/*var edad=prompt("Por favor, ingrese su edad");
+
+//Verificar si la entrada es nula o no es un número
+if(edad===null||isNaN(edad)) {
+    alert("Por favor, ingrese una edad válida.");
+} else{
+    //Convertir la entrada a un número entero
+    edad=parseInt(edad);
+
+//Verificar la edad y mostrar la alerta correspondiente
+if(edad<18){
+    alert("Eres menor de edad.");
+} else if (edad>=18&&edad<65){
+    alert("Eres mayor de edad");
+} else{
+    alert("Eres un adulto mayor.");
+}
+}*/
+
+
+//TIPOS DE DATOS Y VARIABLES
+
+//scope es el alcance de nuestras variables
+
+/*var, let, const la diferencia es el alcance 
+var tiene un alcance global
+let tiene un alcnace local
+const se mantiene sin ningun cambio*/
+
+/*string:cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+let nombre="Romina";
+let invitadxsExtras=2;
+let esMayorEdad=true;
+let edad=24;
+let invitadxEspecial=null;
+let horadDeSalida=undefined;
